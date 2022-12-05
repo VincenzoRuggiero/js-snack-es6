636938409eaacca5c52260d0291ff85a6a53a1aa
VincenzoRuggiero <vincenzoru@live.it>
2022-12-05 12:42:00 +0100

    Push snack 2

---
+++ snack 2/snack02.js
@@ -0,0 +1,57 @@
+const zucchine = [
+  {
+    varietà: "Zucchino nero di Milano",
+    peso: 1.5,
+    lunghezza: 0.6,
+  },
+  {
+    varietà: "Zucchino romanesco",
+    peso: 0.9,
+    lunghezza: 0.8,
+  },
+  {
+    varietà: "Zucchino ortolano di Faenza",
+    peso: 1.2,
+    lunghezza: 0.7,
+  },
+  {
+    varietà: "Zucchina lunga fiorentina",
+    peso: 0.6,
+    lunghezza: 0.6,
+  },
+  {
+    varietà: "Zucchino siciliano",
+    peso: 1.7,
+    lunghezza: 0.6,
+  },
+  {
+    varietà: "Zucchina striata di Napoli",
+    peso: 2.0,
+    lunghezza: 0.55,
+  },
+  {
+    varietà: "Zucchina bianca tristina",
+    peso: 1.5,
+    lunghezza: 0.6,
+  },
+  {
+    varietà: "Zucchina rigata pugliese",
+    peso: 1.4,
+    lunghezza: 0.9,
+  },
+  {
+    varietà: "Zucchino tondo di Piacenza",
+    peso: 2.1,
+    lunghezza: 0.55,
+  },
+  {
+    varietà: "Zucchina tondo di Nizza",
+    peso: 1.45,
+    lunghezza: 0.4,
+  },
+];
+
+for (let i = 0; i < zucchine.length; i++) {
+  let sum = zucchine[i].peso + zucchine[i].peso;
+  console.log(sum);
+}
