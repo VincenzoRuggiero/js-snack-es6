30e4cc3bc6fb610427a0c690bd9e80aba96109b5
VincenzoRuggiero <vincenzoru@live.it>
2022-12-05 11:38:44 +0100

    Push bonus

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ const student = {
 for (let key in student) {
   console.log(key, student[key]);
 }
+
+const studentA = {
+  name: "Paul",
+  surname: "Logan",
+  age: 34,
+};
+
+const studentB = {
+  name: "Ted",
+  surname: "Mosby",
+  age: 30,
+};
+
+const studentC = {
+  name: "Nico",
+  surname: "Robin",
+  age: 29,
+};
+const students = [studentA, studentB, studentC];
+
+for (let i = 0; i < students.length; i++) {
+  console.log(students[i]);
+}
